7f9cca3a838e3461a7f3ae62496ae2fee506dc82
Kenny Jinhiro <jxnhiro@gmail.com>
2024-01-07 03:32:32 +0700

    Finished Fetching Categories

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -3,9 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 namespace BookStore.Controllers;
 
 public class CategoryController : Controller
-{
+{   
+    private readonly ApplicationDbContext _database;
+
+    public CategoryController(ApplicationDbContext database)
+    {
+        _database = database;
+    }
     public IActionResult Index()
     {
+        var fetchedCategoriesList = _database.Categories.ToList();
         return View();
     }
 }
--- BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df07e8ff0276c092fffd0d917c8b2bf24f21304c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d9449e5731759ec3eb5b22bc1a9de114b35138")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
